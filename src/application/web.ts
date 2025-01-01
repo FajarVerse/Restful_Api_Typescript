e52177eeda3f7a18adc14e2f4237a8d3e402e5d5
FajarVerse <gifarifajarm174@gmail.com>
2025-01-01 20:21:46 +0700

    Learning Day 4

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { publicRouter } from "../route/public-api";
 import { ErrorMiddleware } from "../middleware/error-middleware";
+import { apiRouter } from "../route/api";
 
 // Setup Express
 export const web = express();
 web.use(express.json());
 web.use(publicRouter);
 web.use(ErrorMiddleware);
+web.use(apiRouter);
